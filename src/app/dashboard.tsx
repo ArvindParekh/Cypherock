336f907c4f42f72cc9dde671b36d91ca2aa3f1f6
Arvind <aruparekh@gmail.com>
2024-07-10 23:15:51 +0530

    fix: add extra wallet in the sidebar

--- src/app/dashboard.tsx
+++ src/app/dashboard.tsx
@@ -1,3 +1,4 @@
+import { FormControl, MenuItem, Select } from "@mui/material";
 import dashboardData from "../data/dashboard"
 import MaxWidthDialog from "./modal";
 
@@ -16,7 +17,7 @@ export default function Dashboard() {
 
     const walletRow = (
         <div className="w-full flex items-center justify-between mb-3">
-            <h1 className="text-[#C78D4E] text-xl font-bold">Wallet 1</h1>
+            <h1 className="text-[#C78D4E] text-2xl font-extrabold">Wallet 1</h1>
             <button className="flex bg-white/[6%] px-7 py-2 rounded-md text-[#BEB4A8]">
                 + Add Coin
             </button>
@@ -26,7 +27,24 @@ export default function Dashboard() {
     const coinRow = (
         <div className="w-full h-14 flex items-center justify-between border border-[#161C23] px-4">
             <span className="text-[#ADABAA]">Total coin - 6</span>
-            <span className="text-[#BEB4A8]">Amount High - Low</span>
+            {/* <span className="text-[#BEB4A8]">Amount High - Low</span> */}
+            <FormControl sx={{ m: 1, minWidth: 120, color: "white" }}>
+                <Select
+                    value="adfa"
+                    // onChange={handleChange}
+                    label="adfa"
+                    // inputProps={{ 'aria-label': 'Without label' }}
+                    sx={{color: "white"}}
+                >
+                    <MenuItem value="" sx={{color: "white"}} defaultChecked>
+                        Amount High - Low
+                    </MenuItem>
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+                {/* <FormHelperText>Without label</FormHelperText> */}
+            </FormControl>
         </div>
     )
 
@@ -36,8 +54,8 @@ export default function Dashboard() {
 
             {dashboardData.map((data) => {
                 return (<div className="w-full h-20 grid grid-cols-4 gap-3 place-items-center bg-[#161C23] text-[#ADABAA] font-bold">
-                    <div className="flex items-center gap-5">
-                        <img src={data.img} />
+                    <div className="flex items-center gap-2 place-self-stretch ml-10">
+                        <img src={data.img} className="w-12" />
                         <span>{data.name}</span>
                     </div>
                     <div>{data.crypto}</div>
@@ -47,7 +65,6 @@ export default function Dashboard() {
                         <div className="flex items-center gap-3 border-l border-l-[#222D3A] pl-5">
                             <img src="/up_arrow.svg" />
                             <span className="text-[#CAA276] font-bold">SEND</span>
-
                         </div>
                     </div>
                 </div>)
--- src/app/sidebar.tsx
+++ src/app/sidebar.tsx
@@ -4,13 +4,20 @@ const listItems = (
             <img src="/portfolio.svg" className="w-7 h-7"></img>
             <span>Portfolio</span>
         </li>
-        <li className="flex items-center gap-6">
-            <img src="/wallets.svg" className="w-7 h-7"></img>
-            <span className="text-[#E2C19D]">Wallets</span>
+        <li className="flex flex-col">
+            <div className="flex items-center gap-6">
+                <img src="/wallets.svg" className="w-7 h-7"></img>
+                <span className="text-[#E2C19D]">Wallets</span>
+            </div>
+            <div className="flex flex-col ml-16">
+                <span className="font-extrabold text-lg text-[#C78D4E]">Wallet 1</span>
+                <span className="border-2 text-base border-dotted border-[#616161] text-center text-[#616161] rounded-2xl">+ add wallet</span>
+            </div>
         </li>
         <li className="flex items-center gap-6">
             <img src="transactions.svg" className="w-7 h-7"></img>
             <span>Last Transactions</span>
+
         </li>
         <li className="flex items-center gap-6">
             <img src="tutorials.svg" className="w-7 h-7"></img>
